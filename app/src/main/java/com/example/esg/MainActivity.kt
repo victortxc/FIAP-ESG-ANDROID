00e86eac606f6925f4e5185472b2202b2f2d0b12
Victor Medeiros <victormedeiros.pj@gmail.com>
2023-03-07 12:01:33 -0300

    adicionando navegacao ao clicar login

--- app/src/main/java/com/example/esg/MainActivity.kt
+++ app/src/main/java/com/example/esg/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.esg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnLogin: Button = findViewById<Button>(R.id.login_btn)
+
+        btnLogin.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
